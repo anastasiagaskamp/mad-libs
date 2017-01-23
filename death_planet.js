888ae1624635b001bc04f72d30e1e845ea3c5cb9
Anastasia <agaskamp@icloud.com>
2017-01-22 20:51:21 -0500

    Added files

---
+++ death_planet.js
@@ -0,0 +1,31 @@
+/*Declaring your variables
+Otherwise known as a bunch of popup windows :0*/
+var name = prompt("What's your name?");
+var pet = prompt("Animal:");
+var color = prompt("A color: ");
+var verb = prompt("A verb ending in -ing");
+var brother = prompt("The most annoying person you know: ");
+var monument = prompt("A feeling: ");
+var animal = prompt("Another animal:");
+var vehicle = prompt("A form of transport: ");
+var liquid = prompt("A liquid-like substance: ");
+var character = prompt("A character from a book, movie, show, or something else:");
+var food = prompt("Something you like to eat");
+//The actual story...
+document.write("Hello, " + name + ". Today, you and " + brother + " are going to the statue of " + monument + ". ");
+
+document.write("Here, it will be painted " + color + " by a\(n\) " + pet + ". This animal is also " + color + ". ");
+
+document.write("After seeing this, you and " + brother + " start " + verb + " to get away from this strange site. " );
+
+document.write("While " + verb + " to get away, you notice something strange. " + brother + " is a " + animal + "! ");
+
+document.write("Upon discovering this bad news, you take a " + vehicle + " to the planet of the " + food + ". ");
+
+document.write("Here, you soon meet " + character + ", who immediately dies once they see you. You start " + verb + " when they die. ");
+
+document.write("Soon afterward, you discover a sign. This says: <br/>");
+
+document.write("<h1>WELCOME TO DEATH PLANET</h1><br/>");
+
+document.write("You then slowly sink down in the " + liquid + ", drowning to your death.");
