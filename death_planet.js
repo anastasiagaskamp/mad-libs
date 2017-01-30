c8da9d71249febde4c4372c963ba46ae754a0570
Anastasia <agaskamp@icloud.com>
2017-01-29 20:28:39 -0500

    Finished all portions.

--- death_planet.js
+++ death_planet.js
@@ -1,10 +1,10 @@
 /*Declaring your variables
 Otherwise known as a bunch of popup windows :0*/
 var name = prompt("What's your name?");
-var pet = prompt("Animal:");
-var color = prompt("A color: ");
+var pet = prompt("What's your favorite animal?");
+var color = prompt("Favorite color?");
 var verb = prompt("A verb ending in -ing");
-var brother = prompt("The most annoying person you know: ");
+var brother = prompt("A friend or person you know: ");
 var monument = prompt("A feeling: ");
 var animal = prompt("Another animal:");
 var vehicle = prompt("A form of transport: ");
@@ -12,6 +12,7 @@ var liquid = prompt("A liquid-like substance: ");
 var character = prompt("A character from a book, movie, show, or something else:");
 var food = prompt("Something you like to eat");
 //The actual story...
+alert("Hello! This will guide you through a series of popups that will gather the information for the game. It will then put this information into the actual thing.");
 document.write("Hello, " + name + ". Today, you and " + brother + " are going to the statue of " + monument + ". ");
 
 document.write("Here, it will be painted " + color + " by a\(n\) " + pet + ". This animal is also " + color + ". ");
@@ -28,4 +29,6 @@ document.write("Soon afterward, you discover a sign. This says: <br/>");
 
 document.write("<h1>WELCOME TO DEATH PLANET</h1><br/>");
 
-document.write("You then slowly sink down in the " + liquid + ", drowning to your death.");
+document.write("You then slowly sink down into the " + liquid + ", drowning to your death.");
+
+document.write("You wish that your life could end in a more spectacular way, but, sadly, it will not. This is your end.");
